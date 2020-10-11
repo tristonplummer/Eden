d98297653ea212b647db807b1cc6f6d24cda150a
TristonPlummer <me@triston.io>
2020-10-12 02:15:16 +1100

    Add deletion flag to character list

--- eden/common/include/shaiya/common/net/packet/game/CharacterList.hpp
+++ eden/common/include/shaiya/common/net/packet/game/CharacterList.hpp
@@ -23,28 +23,28 @@ namespace shaiya::net
         /**
          * The slot in the character list
          */
-        uint8_t slot { 0 };
+        uint8_t slot{ 0 };
 
         /**
          * The character id
          */
-        uint32_t id { 0 };
+        uint32_t id{ 0 };
 
         /**
          * The timestamp that the character was created. Honestly I'm not sure why the client
          * would / should ever need to know this, so let's just keep it as 0.
          */
-        uint32_t creationTime { 0 };
+        uint32_t creationTime{ 0 };
 
         /**
          * The level of the character
          */
-        uint16_t level { 0 };
+        uint16_t level{ 0 };
 
         /**
          * The character's race.
          */
-        ShaiyaRace race { ShaiyaRace::Human };
+        ShaiyaRace race{ ShaiyaRace::Human };
 
         /**
          * The game mode of the character.
@@ -53,50 +53,50 @@ namespace shaiya::net
          * 2 = Hard
          * 3 = Ultimate
          */
-        ShaiyaGameMode mode { ShaiyaGameMode::Easy };
+        ShaiyaGameMode mode{ ShaiyaGameMode::Easy };
 
         /**
          * The hair style value of the character
          */
-        uint8_t hair { 0 };
+        uint8_t hair{ 0 };
 
         /**
          * The face value of the character
          */
-        uint8_t face { 0 };
+        uint8_t face{ 0 };
 
         /**
          * The height of the character
          */
-        uint8_t height { 0 };
+        uint8_t height{ 0 };
 
         /**
          * The class of the character. We use the name 'job' as that's how it is referred to in the
          * Ep4 files, and because 'class' is a reserved keyword.
          */
-        ShaiyaClass job { ShaiyaClass::Fighter };
+        ShaiyaClass job{ ShaiyaClass::Fighter };
 
         /**
          * The gender of the character
          * 0 = Male
          * 1 = Female
          */
-        uint8_t gender { 0 };
+        uint8_t gender{ 0 };
 
         /**
          * The map id that the character is currently located at
          */
-        uint16_t map { 0 };
+        uint16_t map{ 0 };
 
         /**
          * The strength stat
          */
-        uint16_t strength { 0 };
+        uint16_t strength{ 0 };
 
         /**
          * The dexterity stat
          */
-        uint16_t dexterity { 0 };
+        uint16_t dexterity{ 0 };
 
         /**
          * The reaction (or "REC") stat. I don't like this name, but it's how
@@ -105,52 +105,52 @@ namespace shaiya::net
          *
          * For sake of consistency, we will continue to refer to it as "reaction".
          */
-        uint16_t reaction { 0 };
+        uint16_t reaction{ 0 };
 
         /**
          * The intelligence stat
          */
-        uint16_t intelligence { 0 };
+        uint16_t intelligence{ 0 };
 
         /**
          * The wisdom stat
          */
-        uint16_t wisdom { 0 };
+        uint16_t wisdom{ 0 };
 
         /**
          * The luck stat
          */
-        uint16_t luck { 0 };
+        uint16_t luck{ 0 };
 
         /**
          * The current hitpoints of the character
          */
-        uint16_t hitpoints { 0 };
+        uint16_t hitpoints{ 0 };
 
         /**
          * The current mana of the character
          */
-        uint16_t mana { 0 };
+        uint16_t mana{ 0 };
 
         /**
          * The current stamina of the character
          */
-        uint16_t stamina { 0 };
+        uint16_t stamina{ 0 };
 
         /**
          * The item "types" for the character's worn equipment
          */
-        std::array<char, 17> equipmentTypes { 0 };
+        std::array<char, 17> equipmentTypes{ 0 };
 
         /**
          * The item "type ids" for the character's worn equipment
          */
-        std::array<char, 17> equipmentTypeIds { 0 };
+        std::array<char, 17> equipmentTypeIds{ 0 };
 
         /**
          * Flags that represent if an equipment slot has custom dye.
          */
-        std::array<bool, 17> equipmentDyeFlags { false };
+        std::array<bool, 17> equipmentDyeFlags{ false };
 
         /**
          * The data of the dyed items.
@@ -160,11 +160,16 @@ namespace shaiya::net
         /**
          * The name of the character
          */
-        VariableString<21> name;
+        VariableString<19> name;
+
+        /**
+         * If this character is pending to be deleted.
+         */
+        bool pendingDeletion{ false };
 
         /**
          * The cloak data (emblems, colours, etc)
          */
-        std::array<char, 6> cloakData { 0 };
+        std::array<char, 6> cloakData{ 0 };
     } PACKED;
 }
