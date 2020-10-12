ef0d559dceca545736d8bc44e5037d98cdec5724
TristonPlummer <me@triston.io>
2020-10-13 04:27:16 +1100

    Wrap the command parsing in a try-catch

--- eden/gameserver/src/world/model/commands/CommandManager.cpp
+++ eden/gameserver/src/world/model/commands/CommandManager.cpp
@@ -1,3 +1,4 @@
+#include <shaiya/game/world/model/actor/character/Character.hpp>
 #include <shaiya/game/world/model/commands/CommandManager.hpp>
 #include <shaiya/game/world/model/commands/impl/SpawnItemCommand.hpp>
 #include <shaiya/game/world/model/commands/impl/TeleportCommand.hpp>
@@ -27,29 +28,30 @@ void CommandManager::execute(Character& character, const std::string& text) cons
     using namespace std;
     using namespace boost;
 
-    // Split the string by spaces, unless they are surrounded in quotation marks.
-    vector<string> args;
-    tokenizer<escaped_list_separator<char>> token(text, escaped_list_separator<char>('\\', ' ', '\"'));
-    for (auto itr = token.begin(); itr != token.end(); ++itr)
-        args.push_back(*itr);
-
-    // Erase the first argument, as it's our identifier
-    auto identifier = (*args.begin()).substr(1);
-    args.erase(args.begin());
-    algorithm::to_lower(identifier);
-
-    // If a command with the specified identifier exists, execute it.
-    if (commands_.contains(identifier))
+    try
     {
-        auto command = commands_.at(identifier);
-        try
+        // Split the string by spaces, unless they are surrounded in quotation marks.
+        vector<string> args;
+        tokenizer<escaped_list_separator<char>> token(text, escaped_list_separator<char>('\\', ' ', '\"'));
+        for (auto itr = token.begin(); itr != token.end(); ++itr)
+            args.push_back(*itr);
+
+        // Erase the first argument, as it's our identifier
+        auto identifier = (*args.begin()).substr(1);
+        args.erase(args.begin());
+        algorithm::to_lower(identifier);
+
+        // If a command with the specified identifier exists, execute it.
+        if (commands_.contains(identifier))
         {
+            auto command = commands_.at(identifier);
             command->execute(character, args);
         }
-        catch (const std::exception& e)
-        {
-            LOG(INFO) << "Command with identifier \"" << identifier << "\" threw an exception: " << e.what();
-        }
+    }
+    catch (const std::exception& e)
+    {
+        LOG(INFO) << "Command executed by \"" << character.name() << "\" with text: \"" << text
+                  << "\" threw an exception: " << e.what();
     }
 }
 
